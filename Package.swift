5e089e370f6071970550352378195d551126e360
dmitriyNikolin-cb <dmitriy.nikolin@cleverbase.com>
2022-10-04 12:06:29 +0300

    release v4.6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "opencv2",
-                      url: "https://github.com/Yeatse/OpenCV-SPM/releases/download/\(version)/opencv2.xcframework.zip",
+                      url: "https://github.com/dmitriyNikolin-cb/OpenCV-SPM/releases/download/\(version)/opencv2.xcframework.zip",
                       checksum: checksum),
     ]
 )
